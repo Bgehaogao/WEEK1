2936d4f3ad321aa4b0d4e6ffb8258b717e49a517
Izumi Sakai <131006166+E1Richard@users.noreply.github.com>
2024-08-21 11:22:47 +0800

    V1.0
    就是个框架

---
+++ 组队小程序/pages/index/index.js
@@ -0,0 +1,13 @@
+// index.js
+Page({
+  navigateToTarget1() {
+    wx.navigateTo({
+      url: 'pages/teacher/teacher'
+    });
+  },
+  navigateToTarget2() {
+    wx.navigateTo({
+      url: 'pages/student/student'  
+    });
+  }
+});
